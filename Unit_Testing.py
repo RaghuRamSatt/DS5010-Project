68b6635edd7d49e9fe3ca7f692da02030416088a
hasankhwaja <128183942+hasankhwaja@users.noreply.github.com>
2023-04-14 15:11:39 -0400

    Update Unit_Testing.py

--- Unit_Testing.py
+++ Unit_Testing.py
@@ -4,6 +4,7 @@ import Probability
 import hypothesis_testing as ht 
 import parameter_estimation as pe
 import random_sampling as rs 
+import read_file as rv
 import unittest
 
 def test_variance_testing():
@@ -137,6 +138,18 @@ def test_random_sampling():
     random_test.append(generate)
     
     return random_test
+
+def test_files():
+    
+    file_test = []
+    
+    txt = rv.read_data_file('numbers.txt')
+    csv = rv.read_data_csv('more_numbers.csv')
+    
+    file_test.append(txt)
+    file_test.append(csv)
+    
+    return file_test
     
 class TestBinomialDistribution(unittest.TestCase):
     
@@ -197,6 +210,11 @@ class TestBinomialDistribution(unittest.TestCase):
         self.assertEqual(random_test[0], 1)
         self.assertEqual(random_test[1], 0)
         self.assertEqual(random_test[2], [3, 9, 6, 2, 2])
+        
+    def test_read_file(self):
+        file_test = test_files()
+        self.assertEqual(file_test[0], [1, 3, 99, 100, 120, 32, 330, 23, 76, 44, 31])
+        self.assertEqual(file_test[1], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     
 if __name__ == "__main__":
     unittest.main(argv=['first-arg-is-ignored'], exit = False, verbosity = 3)
