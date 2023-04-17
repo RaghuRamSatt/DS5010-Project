9300a053065b9933d7571d85dfd940b8bd1681ed
RaghuRamSatt <sattanapalle.r@northeastern.edu>
2023-04-17 19:34:55 -0400

    Updated 1. Probability.py 2. descriptive_statistics.py 3. random_sampling.py 4. parameter_estimation.py 5. hypothesis_testing.py 6. simulation.py 7. Visualisation.py (combined line chart and bar chart into the two functions)

--- parameter_estimation.py
+++ parameter_estimation.py
@@ -86,6 +86,11 @@ def confidence_interval_normal_approximation(sample_data, confidence_level=0.95)
     _, p_estimate = estimate_parameters(sample_data)
     standard_error = math.sqrt(p_estimate * (1 - p_estimate) / n)
 
+    if p_estimate < 0 or p_estimate > 1:
+        raise ValueError("Estimated proportion is outside the allowed range.")
+    if n <= 0:
+        raise ValueError("Sample size is non-positive.")
+
     # z_score = abs(np.percentile(np.random.standard_normal(100000), (1 - confidence_level) / 2 + confidence_level *
     # 100))
     z_score = norm.ppf(1 - (1 - confidence_level) / 2)
@@ -124,7 +129,8 @@ def confidence_interval_clopper_pearson(sample_data, confidence_level=0.95):
 def confidence_interval_agresti_coull(sample_data, confidence_level=0.95):
     """
     Calculate the confidence interval for the probability of success (p) in a binomial distribution
-    using the Agresti-Coull method.
+    using the Agresti-Coull method. This approach assumes that the sample data represents the number of successes in
+    each trial, not the actual outcomes of each trial.
 
     :param sample_data: (list)  A list of binomial samples with the same number of trials and success probability
     :param confidence_level: (float, optional) The desired confidence level (default is 0.95)
@@ -135,17 +141,23 @@ def confidence_interval_agresti_coull(sample_data, confidence_level=0.95):
         raise ValueError("Sample data must be a non-empty list.")
     if not all(isinstance(d, int) for d in sample_data):
         raise ValueError("All sample data must be integers.")
-    if not all(0 <= d <= 1 for d in sample_data):
-        raise ValueError("All sample data must be either 0 or 1.")
     if not 0 <= confidence_level <= 1:
         raise ValueError("Confidence level must be between 0 and 1.")
 
-    n = len(sample_data)
-    _, p_estimate = estimate_parameters(sample_data)
+    trials = len(sample_data)
+    successes = sum(sample_data)
+
+    # n = len(sample_data)
+    p_estimate = successes / trials
     z_score = norm.ppf(1 - (1 - confidence_level) / 2)
 
-    adjusted_n = n + z_score ** 2
-    adjusted_p = (sum(sample_data) + (z_score ** 2) / 2) / adjusted_n
+    adjusted_n = trials + z_score ** 2
+    adjusted_p = (successes + (z_score ** 2) / 2) / adjusted_n
+
+    if adjusted_p < 0 or adjusted_p > 1:
+        raise ValueError("Adjusted proportion is outside the allowed range.")
+    if adjusted_n <= 0:
+        raise ValueError("Adjusted sample size is non-positive.")
 
     standard_error = math.sqrt(adjusted_p * (1 - adjusted_p) / adjusted_n)
     margin_of_error = z_score * standard_error
