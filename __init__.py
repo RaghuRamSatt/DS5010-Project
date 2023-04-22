72923b83f5ec8b6eb5badf465c8af96c230f8137
RaghuRamSatt <sattanapalle.r@northeastern.edu>
2023-04-22 13:22:56 -0400

    Updated 1. Probability.py 2. descriptive_statistics.py 3. random_sampling.py 4. parameter_estimation.py 5. hypothesis_testing.py 6. simulation.py 7. Visualisation.py (combined line chart and bar chart into the two functions)
    Moved unit tests

--- __init__.py
+++ __init__.py
@@ -1,30 +1,12 @@
-from Probability import validate_parameters, factorial, binomial_coefficient, pmf, cdf
-from descriptive_statistics import (
-    validate_parameters,
-    mean,
-    variance,
-    standard_deviation,
-    mode,
-    skewness,
-    kurtosis,
-    entropy
-)
-from random_sampling import bernoulli_trial, binomial_sample, generate_binomial_samples
-from parameter_estimation import (
-    estimate_parameters,
-    log_likelihood,
-    mle_estimate_parameters,
-    confidence_interval_normal_approximation,
-    confidence_interval_clopper_pearson,
-    confidence_interval_agresti_coull
-)
-from hypothesis_testing import (
-    proportion_z_test,
-    power_analysis_binomial_proportions,
-    fishers_exact_test,
-    chi_square_test,
-    g_test_goodness_of_fit
-)
+from Probability import *
+
+from descriptive_statistics import *
+
+from random_sampling import *
+from parameter_estimation import *
+
+from hypothesis_testing import *
+
 
 # Example usage
 n = 10
