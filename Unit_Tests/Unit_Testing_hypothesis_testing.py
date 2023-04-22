8e1dc841ccbc711c6d1ce10037afdefcc412242f
RaghuRamSatt <sattanapalle.r@northeastern.edu>
2023-04-22 13:49:00 -0400

    Updated
    Moved unit tests and added directory finding and running clean

--- Unit_Tests/Unit_Testing_hypothesis_testing.py
+++ Unit_Tests/Unit_Testing_hypothesis_testing.py
@@ -1,3 +1,9 @@
+import sys
+import os
+current_directory = os.getcwd()
+main_directory_path = os.path.abspath(os.path.join(current_directory, '..'))
+sys.path.insert(0, main_directory_path)
+
 import unittest
 import numpy as np
 from hypothesis_testing import *
